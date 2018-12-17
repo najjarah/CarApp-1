7bfb87b7ce41f4835d972810bf4d772a0eb0152e
Malik Abed Alhade <malikabedelhade@gmail.com>
2018-12-17 19:03:30 +0200

    First Initial commit

---
+++ src/main/java/com/example/car/CarApp/CarAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.car.CarApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarAppApplication.class, args);
+	}
+
+}
+
