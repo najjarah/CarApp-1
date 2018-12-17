7e1cbbd97a7731dc771b978d471478a912dbce33
Malik Abed Alhade <malikabedelhade@gmail.com>
2018-12-17 20:22:37 +0200

    static content added

--- src/main/java/com/example/car/CarApp/CarAppApplication.java
+++ src/main/java/com/example/car/CarApp/CarAppApplication.java
@@ -2,12 +2,18 @@ package com.example.car.CarApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class CarAppApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(CarAppApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CarAppApplication.class, args);
+		//note and save to run again when just saving!
+		logger.info("Hello Spring Boot");
 	}
 
 }
